2a80c9fb177a03aa249c00a31bd6232c4e8c49ba
stkom106 <stipe.kompljenovic@nanoco.hr>
2024-03-24 23:56:34 +0500

    add url for backend

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
 
 // const URL = 'https://skupstina.azurewebsites.net/';
-const URL = process.env.REACT_APP_SOCKET_URL || "http://52.158.47.57:4000/";
+const URL =
+  process.env.REACT_APP_SOCKET_URL ||
+  "https://backend-eskupstina.azurewebsites.net/";
 
 export const socket = io(URL);
