e1415016838fa33da2641a6399a3ad88951e0ed6
FIRST_NAME LAST_NAME <ppavle@live.com>
2024-04-27 13:51:09 +0200

    Deploy fix2

--- src/pages/main/MainScene.js
+++ src/pages/main/MainScene.js
@@ -264,7 +264,7 @@ export default function MainScene(props) {
     const getUsers = async () => {
       const userId = localStorage.getItem("userId");
       const resp = await getUser({ id: userId });
-      // Check if the userId exists in localStoragee
+      // Check if the userId exists in localStorage
       if (userId) {
         // Find the user with the matching ID
         const user = resp.data.find((user) => user._id === userId);
