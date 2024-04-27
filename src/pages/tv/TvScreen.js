de3a8eb0632494cc2977eaff3c56bf3def5e83a9
ppavle97 <40036565+ppavle97@users.noreply.github.com>
2024-04-27 02:18:45 +0200

    Update TvScreen.js

--- src/pages/tv/TvScreen.js
+++ src/pages/tv/TvScreen.js
@@ -16,6 +16,7 @@ export default function LoginScene() {
   const [agendaId, setAgendaId] = useState("");
   const [updateChange, setUpdateChange] = useState(false);
   const [agenda, setAgenda] = useState("");
+  
 
   useEffect(() => {
     socket.on("live_voting_results", (agendaId) => {
