4feb11057b4818b5a6c9add040f923756ad5c952
krishna <krishna.agarwal961@gmail.com>
2024-07-22 14:04:50 +0530

    baseurl updated

--- src/services/axios.js
+++ src/services/axios.js
@@ -7,8 +7,7 @@ import axios from "axios";
 
 axios.defaults.baseURL =
   process.env.REACT_APP_API_URL ||
-  // "https://backend-eskupstina.azurewebsites.net/";
-  "http://localhost:5005/"
+  "https://backend-eskupstina.azurewebsites.net/";
 
 /*  axios.defaults.baseURL = "http://localhost:5005/";
  */ const signInUser = async (props) => {
