6f34669331d8ba59a2da3e70c42d90ce388dfa52
jitudevloper <jitendrayadav7073@gmail.com>
2024-07-22 13:28:02 +0530

    add pop

--- src/components/Statistics/AgendasTable.js
+++ src/components/Statistics/AgendasTable.js
@@ -1,10 +1,13 @@
-import {  MagnifyingGlassIcon} from "@heroicons/react/24/outline";
+import { EyeIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import {
   Button,
   Card,
   CardBody,
   CardFooter,
   CardHeader,
+  Dialog,
+  DialogBody,
+  DialogFooter,
   IconButton,
   Input,
   Typography,
@@ -15,18 +18,19 @@ import { agendasList } from "../../services/axios";
 
 export default function AgendasTable() {
 
-  const TABLE_HEAD = ["Sr.No.", "Name", "Description", "Type", "Voters" ];
+  const TABLE_HEAD = ["Sr.No.", "Name", "Description", "Type", "Voters"];
   const [page, setPage] = useState(1);
   const [search, setSearch] = useState("");
   const [totalPage, setTotalPage] = useState(1);
   const [users, setUsers] = useState([]);
+  const [openModal, setOpenModal] = useState(false);
 
   useEffect(() => {
-    getagendasList(page,search );
-  }, [page,search]);
+    getagendasList(page, search);
+  }, [page, search]);
 
-  const getagendasList = async (page,search) => {
-    const resp = await agendasList(page,search);
+  const getagendasList = async (page, search) => {
+    const resp = await agendasList(page, search);
     setUsers(resp?.data);
     setTotalPage(resp?.totalPages);
     setPage(resp?.currentPage);
@@ -42,13 +46,19 @@ export default function AgendasTable() {
     }
   };
 
+  const openVotersModal = () => {
+    setOpenModal(true);
+  };
+  const closeVotersModal = () => {
+    setOpenModal(false);
+  };
   return (
     <Card className="table_background">
       <CardHeader floated={false} shadow={false} className="rounded-none">
         <div className="mb-8 flex items-center justify-between gap-8">
           <div>
             <Typography variant="h5" color="blue-gray">
-            Agenda list
+              Agenda list
             </Typography>
             <Typography color="gray" className="mt-1 font-normal">
               See information about agenda
@@ -61,7 +71,7 @@ export default function AgendasTable() {
               label="Search"
               icon={<MagnifyingGlassIcon className="h-5 w-5" />}
               value={search}
-              onChange={(e)=> setSearch(e.target.value)}
+              onChange={(e) => setSearch(e.target.value)}
             />
           </div>
         </div>
@@ -92,7 +102,7 @@ export default function AgendasTable() {
               const classes = isLast
                 ? "p-4"
                 : "p-4 border-b border-blue-gray-50";
-                { console.log(item, 'items') }
+              { console.log(item, 'items') }
               return (
                 <tr key={index}>
                   <td className={classes}>
@@ -145,8 +155,16 @@ export default function AgendasTable() {
                           color="blue-gray"
                           className="font-normal w-40 opacity-70"
                         >
-                          {item?.voters?.length || 0 }
+                          {item?.voters?.length || 0}
+                          <Button
+                            className="ml-2 bg-blue-500 text-white rounded p-2"
+                            onClick={() => openVotersModal(item?.voters)}
+                          >
+                            <EyeIcon className="h-5 w-5" />
+                          </Button>
                         </Typography>
+
+
                       </div>
                     </div>
                   </td>
@@ -179,6 +197,114 @@ export default function AgendasTable() {
           Next
         </Button>
       </CardFooter>
+
+      <Dialog open={openModal} onClose={closeVotersModal} fullWidth maxWidth="lg">
+        <DialogBody
+          divider
+          style={{
+            maxHeight: '80vh',
+            overflowY: 'auto',
+            padding: '16px',
+          }}
+        >
+          <div style={{ overflowY: 'auto', width: '100%' }}>
+            <table style={{ width: '100%', borderCollapse: 'collapse' }}>
+              <thead>
+                <tr>
+                  {TABLE_HEAD.map((head) => (
+                    <th
+                      key={head}
+                      className="border-y border-blue-gray-100 bg-blue-gray-50/50 p-4"
+                    >
+                      <Typography
+                        variant="small"
+                        color="blue-gray"
+                        className="font-normal leading-none opacity-70"
+                      >
+                        {head}
+                      </Typography>
+                    </th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {users.map((item, index) => {
+                  const isLast = index === users.length - 1;
+                  const classes = isLast
+                    ? "p-4"
+                    : "p-4 border-b border-blue-gray-50";
+                  return (
+                    <tr key={index}>
+                      <td className={classes}>
+                        <div className="flex items-center gap-3">
+                          <Typography
+                            variant="small"
+                            color="blue-gray"
+                            className="font-bold"
+                          >
+                            {index + 1}
+                          </Typography>
+                        </div>
+                      </td>
+                      <td className={classes}>
+                        <Typography
+                          variant="small"
+                          color="blue-gray"
+                          className="font-normal"
+                        >
+                          {item?.user?.name}
+                        </Typography>
+                      </td>
+                      <td className={classes}>
+                        <Typography
+                          variant="small"
+                          color="blue-gray"
+                          className="font-normal"
+                        >
+                          {item?.user?.description}
+                        </Typography>
+                      </td>
+                      <td className={`${classes} col-2`}>
+                        <div className="flex items-center gap-3">
+                          <Typography
+                            variant="small"
+                            color="blue-gray"
+                            className="font-normal"
+                          >
+                            {item?.user?.agenda_type}
+                          </Typography>
+                        </div>
+                      </td>
+                      <td className={`${classes} col-2`}>
+                        <div className="flex items-center gap-3">
+                          <Typography
+                            variant="small"
+                            color="blue-gray"
+                            className="font-normal"
+                          >
+                            {item?.voters?.length || 0}
+                          </Typography>
+                        </div>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </DialogBody>
+        <DialogFooter>
+          <Button
+            variant="text"
+            color="red"
+            onClick={closeVotersModal}
+            className="text-red-500"
+          >
+            Close
+          </Button>
+        </DialogFooter>
+      </Dialog>
+
     </Card>
   );
 }
