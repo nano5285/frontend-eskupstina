f687160dfcf3b42fa109709202dbc0fd49c61098
vivekworks114 <vivekworks114@gmail.com>
2025-04-01 19:44:00 +0530

    Update socket connection URL and add deployment configuration

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -3,6 +3,7 @@ import { io } from "socket.io-client";
 // const URL = 'https://skupstina.azurewebsites.net/';
 const URL =
   process.env.REACT_APP_SOCKET_URL ||
-  "https://backend-eskupstina.azurewebsites.net/";
+  // "https://backend-eskupstina.azurewebsites.net/";
+  "https://e-skupstina-backend.azurewebsites.net/"
 
 export const socket = io(URL);
